e68b03ba99d50a3f896e91cca64bc538ffd19bc0
larissa <larissa.queiroz95@gmail.com>
2022-10-19 22:07:43 -0300

    added dao models

---
+++ Free_API/Free_API/Models/DAO/Dish.cs
@@ -0,0 +1,10 @@
+﻿namespace Free_API.Models.DAO
+{
+    public class Dish
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double price { get; set; }
+        public string description { get; set; }
+    }
+}
